b3e05108463da3d7b14459f69e9eca02bde99728
pravlearn <115153400+pravlearn@users.noreply.github.com>
2022-12-18 23:53:17 +0530

    Add files via upload

---
+++ IloginRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI.WebControls;
+using CarWashWebApiService.Interface;
+using CarWashWebApiService.Models;
+
+
+namespace CarWashWebApiService.Repository
+{
+    public class IloginRepository : Ilogin
+    {
+        CarWashEntities carWashDatabaseContext = new CarWashEntities();
+
+        public Customer CustomerLogin(UserLogin login)
+        {
+
+            var Role = new Customer()
+            {
+                Email = login.Email,
+                Password = login.Password,
+                Role = "Customer"
+            };
+            var customerData= carWashDatabaseContext.Customers.Where(u => u.Email == Role.Email && u.Password == Role.Password && u.Role == Role.Role).SingleOrDefault();
+
+            var user = new Customer()
+            {
+                Id = customerData.Id,
+                FirstName = customerData.FirstName,
+                LastName = customerData.LastName,
+                Email = customerData.Email,
+                PhoneNumber = customerData.PhoneNumber,
+                Role = customerData.Role,
+            };
+            return user;
+        }
+
+        public Customer WasherLogin(UserLogin login)
+        {
+            var Role = new Customer()
+            {
+                Email = login.Email,
+                Password = login.Password,
+                Role = "Washer"
+            };
+            var washerData = carWashDatabaseContext.Customers.Where(u => u.Email == Role.Email && u.Password == Role.Password && u.Role == Role.Role).SingleOrDefault();
+
+            var user = new Customer()
+            {
+                Id = washerData.Id,
+                FirstName = washerData.FirstName,
+                LastName = washerData.LastName,
+                Email = washerData.Email,
+                PhoneNumber = washerData.PhoneNumber,
+                Role = washerData.Role,
+            };
+            return user;
+        }
+   
+    }
+}
---
+++ UserRepository.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using CarWashWebApiService.Interface;
+using CarWashWebApiService.Models;
+
+namespace CarWashWebApiService.Repository
+{
+    public class UserRepository : IUser<Customer>
+    {
+        CarWashEntities carWashDatabaseContext = new CarWashEntities();
+
+        
+        public Customer CreateUser(Customer customer)
+        {
+
+            try
+            {
+                var newuser = new Customer()
+                {
+                    FirstName = customer.FirstName,
+                    LastName = customer.LastName,
+                    Email = customer.Email,
+                    PhoneNumber = customer.PhoneNumber,
+                    Password = customer.Password,
+                    Role = customer.Role
+                };
+
+                if (carWashDatabaseContext != null)
+                {
+                    carWashDatabaseContext.Customers.Add(newuser);
+                    carWashDatabaseContext.SaveChanges();
+                    return newuser;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return null;
+        }
+
+        public void Delete(int id)
+        {
+            Customer customer = carWashDatabaseContext.Customers.Find(id);
+            carWashDatabaseContext.Customers.Remove(customer);
+            carWashDatabaseContext.SaveChanges();
+        }
+
+        public List<Customer> GetAllUsers()
+        {
+            return carWashDatabaseContext.Customers.ToList();
+
+        }
+
+        public List<Customer> GetAllWashers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetUserbyEmail(int email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetUserbyId(int email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetUserId(int id)
+        {
+            if (carWashDatabaseContext != null)
+            {
+                var customer = from c in carWashDatabaseContext.Customers where c.Id == id select c;
+                
+                return customer;
+            }
+            return null;
+        }
+    }
+}
